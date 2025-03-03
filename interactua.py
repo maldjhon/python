ed467e8cd6384a07c95c139cbf5be5a6852cebc6
maldjhon <jfmaldonado26@gmail.com>
2025-03-03 00:57:47 -0500

    Segundo ajuste interaccion

--- interactua.py
+++ interactua.py
@@ -12,7 +12,7 @@ service = Service(r'D:\Documentos\Proyectos\chromedriver-win64\chromedriver.exe'
 driver = webdriver.Chrome(service=service, options=options)
 
 # Abre la página web
-driver.get('https://maldjhon.github.io/userinterface/')
+driver.get('http://localhost:3001/')
 
 # Espera hasta que los elementos de la página estén cargados
 time.sleep(2)
@@ -22,27 +22,29 @@ campo_usuario = driver.find_element(By.ID, 'usuario')
 campo_contrasena = driver.find_element(By.ID, 'contrasena')
 
 # Ingresa el usuario y la contraseña
-campo_usuario.send_keys('jhon')
-campo_contrasena.send_keys('jhon1')
+campo_usuario.send_keys('jmaldonado')
+campo_contrasena.send_keys('jhon123')
 
 # Encuentra el botón de ingresar y haz clic en él
 boton_ingresar = driver.find_element(By.ID, 'btnIngresar')
 boton_ingresar.click()
 
 # Espera unos segundos para observar el resultado
-time.sleep(120)
+time.sleep(20)
 
-# Captura el mensaje de alerta si aparece
-try:
-    alerta = driver.switch_to.alert
-    mensaje_alerta = alerta.text
-    print(f"Mensaje de alerta: {mensaje_alerta}")
-    alerta.accept()  # Acepta la alerta para cerrarla
-except NoAlertPresentException:
-    print("No hay alerta presente.")
+# Capturar la URL actual
+current_url = driver.current_url
+driver.get(current_url)
 
-# Espera unos segundos para observar el resultado
-time.sleep(60)
+# Captura el campo nombre en la nueva pantalla
+campo_nombre = driver.find_element(By.ID,'nombre')
+campo_nombre.send_keys('PRUEBA')
+
+# Encuenta el botón field y haz clic en él
+boton_selec_archivo = driver.find_element(By.ID, 'archivo')
+# Sube un archivo
+boton_selec_archivo.send_keys(r'C:\Users\jhonf\Downloads\evidencia_humedad.jpg')
+time.sleep(10)
 
 # Cierra el navegador
 driver.quit()
